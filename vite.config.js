cd11f5a7e5e6dc53a767146d1f0912ed2e1c9cca
kharidubard <kharidubard@gmail.com>
2024-06-27 18:10:59 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dubardPortfolio/', // Replace 'dubardPortfolio' with your actual repository name if different
+  //base: '/dubardPortfolio/', // Replace 'dubardPortfolio' with your actual repository name if different
   build: {
     outDir: 'dist',
     rollupOptions: {
