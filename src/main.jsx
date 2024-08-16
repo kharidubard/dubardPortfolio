90ed403901a5001f3d6fbb1b9c236e50c2b8236d
kharidubard <kharidubard@gmail.com>
2024-08-16 14:03:40 -0400

    Update main.jsx
    Hash Router Test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home.jsx'
-import Base from './pages/Base.jsx'
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Base from './pages/Base.jsx';
 import Projects from './pages/ProjectsPage.jsx';
 import Contact from './pages/ContactPage.jsx';
-import './index.css'
+import './index.css';
 import NPS_PNW from './pages/projects/NPS_PNW.jsx';
 import DCSite from './pages/projects/dcSite.jsx';
 import Designs from './pages/DesignsPage.jsx';
@@ -15,21 +15,22 @@ import DC_Web from './pages/designs/DC_webdesigns.jsx';
 import InternPage from './pages/designs/InternPageRedesign.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Base />}>
         <Route index element={<Home />} />
-        <Route path="/projects" element={<Projects />}/>
-        <Route path="/designs" element={<Designs />}/>
-        <Route path="/projects/NPS_PNW" element={<NPS_PNW />} />
-        <Route path="/projects/dcSite" element={<DCSite />} />
-        <Route path="/projects/Portfolio" element={<PortfolioSite />} />
-        <Route path="/designs/DC_Branding" element={<DCBrand />} />
-        <Route path="/designs/Task_Page" element={<TaskPage />} />
-        <Route path="/designs/DC_webdesigns" element={<DC_Web />} />
-        <Route path="/designs/InternPageRedesign" element={<InternPage />} />
-        <Route path="/contact" element={<Contact />}/>
+        <Route path="projects" element={<Projects />} />
+        <Route path="designs" element={<Designs />} />
+        <Route path="projects/NPS_PNW" element={<NPS_PNW />} />
+        <Route path="projects/dcSite" element={<DCSite />} />
+        <Route path="projects/Portfolio" element={<PortfolioSite />} />
+        <Route path="designs/DC_Branding" element={<DCBrand />} />
+        <Route path="designs/Task_Page" element={<TaskPage />} />
+        <Route path="designs/DC_webdesigns" element={<DC_Web />} />
+        <Route path="designs/InternPageRedesign" element={<InternPage />} />
+        <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
-  </BrowserRouter>,
-)
+  </HashRouter>
+);
+
