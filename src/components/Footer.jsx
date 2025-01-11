eae6afbc7f1fb8cdf22f2a42c92ba04b87b74aff
kharidubard <kharidubard@gmail.com>
2025-01-11 17:37:43 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="bg-primaryOrange text-white py-4">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row md:justify-between md:items-center">
-          <p className="text-[14px] md:text-base mb-4 md:mb-0">© 2024 Created by Khari DuBard</p>
+          <p className="text-[14px] md:text-base mb-4 md:mb-0">© 2025 Created by Khari DuBard</p>
           <ul className="flex flex-wrap justify-center md:justify-end space-x-4">
           <NavLink
 								to="/"
